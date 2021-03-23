6282793a69787bb46c8e56eb6f342d5c1c8eede4
CHIHAB09 <web2020.chihab@gmail.com>
2021-03-23 11:34:36 +0100

    Remove duplication by using app layout

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('base') 
+<!--on appel le template de base-->
 
-        <title>Hello City</title>
-
-    </head>
+@section('content')
     <body>
 
         <p>First test with Laravel &hearts;</p>    
+
         <p><a href="/">Return at home.</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection

7e3792fca6925ec36b3dd06c24dbe87f22a92029
CHIHAB09 <web2020.chihab@gmail.com>
2021-03-25 13:57:59 +0100

    Make use of Route::view instead of Route ::get

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route:: get('/',function(){
+/* Route:: get('/',function(){
     return view('page/home');// le . ou le slash est identique
 })->name('app_home');
 
@@ -12,4 +12,13 @@ Route:: get('/about-us',function(){
 
 Route:: get('/help',function(){
     return view('page/help');
-})->name('app_help');
+})->name('app_help'); */
+
+//ca c'est une maniere plus courte d'ecrire les routes tjrs php
+Route:: get('/',fn() => view('page/home'))->name('app_home');
+
+Route:: get('/about-us',fn() => view('page/about'))->name('app_about');
+
+//encore plus court ecriture laravel
+Route::view('/help','page/help')->name('help');
+

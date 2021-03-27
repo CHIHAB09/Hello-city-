72e2ad7f858f9e7cd0dbb1cfa27d64d22195fc25
CHIHAB09 <web2020.chihab@gmail.com>
2021-03-27 20:39:28 +0100

    Modification route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 /* Route:: get('/',function(){
     return view('page/home');// le . ou le slash est identique
 })->name('app_home');
+*/
+
+//ca c'est une maniere plus courte d'ecrire les routes tjrs php
+Route:: get('/',fn() => view('page.home'))->name('app_home');
+
 
 Route:: get('/about-us',function(){
     return view('page/about');
@@ -12,13 +17,11 @@ Route:: get('/about-us',function(){
 
 Route:: get('/help',function(){
     return view('page/help');
-})->name('app_help'); */
+})->name('app_help'); 
 
-//ca c'est une maniere plus courte d'ecrire les routes tjrs php
-Route:: get('/',fn() => view('page/home'))->name('app_home');
-
+/* 
 //encore plus court ecriture laravel
-Route::view('/about','page/about')->name('app_about');
+Route::view('/about','page.about')->name('app_about');
 
-Route::view('/help','page/help')->name('help');
+Route::view('/help','page.help')->name('help'); */
 
